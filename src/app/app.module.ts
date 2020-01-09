82df1b27234acefc936dd5ec0e134320964c8bee
itsswill <willn2doit@gmail.com>
2020-01-09 16:54:39 -0500

    imported necessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { GameComponent } from './components/game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule
